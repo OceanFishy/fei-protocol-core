040e5a8702f16b76f3aff5094c23a9204e0f8456
OceanFishy <86115816+OceanFishy@users.noreply.github.com>
2021-12-02 12:14:09 -0500

    Update fip-46.ts

--- proposals/dao/fip-46.ts
+++ proposals/dao/fip-46.ts
@@ -1,8 +1,9 @@
 import { ProposalDescription } from '@custom-types/types';
 
-const fip_38: ProposalDescription = {
-  title: 'FIP-38: Tokemak Reactor and Swap',
+const fip_50: ProposalDescription = {
+  title: 'FIP-50: ETH Yield Improvements',
   commands: [
+   //AAVE
     {
       target: 'aaveEthPCVDeposit',
       values: '0',
@@ -24,7 +25,8 @@ const fip_38: ProposalDescription = {
       arguments: ['{ethLidoPCVDeposit}', '11050000000000000000000'],
       description: 'Unwrap 11050 WETH from aaveEthPCVDeposit and send 11050 ETH to Lido stETH deposit'
     },
-  {
+  //Compound
+    {
       target: 'compoundEthPCVDeposit',
       values: '0',
       method: 'withdraw(address,uint256)',
@@ -38,6 +40,7 @@ const fip_38: ProposalDescription = {
       arguments: ['{ethLidoPCVDeposit}', '11050000000000000000000'],
       description: 'Withdraw 11050 ETH from Compound deposit to Lido stETH deposit'
     },
+   //Tokemak
     {
       target: 'ethTokemakPCVDeposit',
       values: '0',
@@ -56,10 +59,10 @@ The Curve stETH-ETH pair has 5.5B worth of liquidity. This marks stETH as being 
 Tokemak is a newer DeFi protocol than stETH, and the TOKE token is less liquid and more unstable than ETH/stETH. As rewards for Tokemak farming are paid in the TOKE token, I recommend a smaller allocation for Tokemak.
 The farming rewards for one year can be sold for 1.8% slippage with current liquidity. As we are staking pure ETH, liquidity for the deposited token is not a concern. 
 The additional TOKE also could allow the DAO to exercise more control over Tokemak, a possibly crucial DeFi primitive.
-Snapshot: 
+Snapshot: https://snapshot.fei.money/#/proposal/0xbf2dfc7c7bcbeae61ec3edf8bf449ff3f440b016da8c0b8be8228aa8f3fa7d05
 Forum discussion: https://tribe.fei.money/t/fip-xx-eth-yield-improvements/3722
 Code: https://github.com/fei-protocol/fei-protocol-core/pull/283
 `
 };
 
-export default fip_38;
+export default fip_50;
